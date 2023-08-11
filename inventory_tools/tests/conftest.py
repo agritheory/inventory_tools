563171b2f44873577d749e709ff6cd84c785761d
Tyler Matteson <tyler@agritheory.com>
2023-08-11 16:44:52 -0400

    test: fix conftest logger issue

--- inventory_tools/tests/conftest.py
+++ inventory_tools/tests/conftest.py
@@ -1,10 +1,23 @@
-import os
 from pathlib import Path
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock
 
 import frappe
 import pytest
 from frappe.utils import get_bench_path
+
+
+def _get_logger(*args, **kwargs):
+	from frappe.utils.logger import get_logger
+
+	return get_logger(
+		module=None,
+		with_more_info=False,
+		allow_site=True,
+		filter=None,
+		max_size=100_000,
+		file_count=20,
+		stream_only=True,
+	)
 
 
 @pytest.fixture(scope="module")
@@ -14,13 +27,9 @@ def monkeymodule():
 
 
 @pytest.fixture(scope="session", autouse=True)
-def mock_settings_env_vars():
-	with patch.dict(os.environ, {"FRAPPE_STREAM_LOGGING": "True"}):
-		yield
+def db_instance():
+	frappe.logger = _get_logger
 
-
-@pytest.fixture(scope="session", autouse=True)
-def db_instance(mock_settings_env_vars):
 	currentsite = "test_site"
 	sites = Path(get_bench_path()) / "sites"
 	if (sites / "currentsite.txt").is_file():
