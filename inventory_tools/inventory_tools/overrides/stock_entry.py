dc01b2432b3a51d99f5818b9461b7fecb0c1cbf0
fproldan <franciscoproldan@gmail.com>
2024-01-24 09:17:31 -0300

    feat: improve message

--- inventory_tools/inventory_tools/overrides/stock_entry.py
+++ inventory_tools/inventory_tools/overrides/stock_entry.py
@@ -3,24 +3,24 @@ import frappe
 
 @frappe.whitelist()
 def get_production_item_if_work_orders_for_required_item_exists(stock_entry_name: str) -> str:
-    stock_entry = frappe.get_doc("Stock Entry", stock_entry_name)
-    
-    if stock_entry.docstatus != 1 or stock_entry.stock_entry_type != "Manufacture":
-        return ""
-    
-    production_item = frappe.get_value("Work Order", stock_entry.work_order, "production_item")
-    WorkOrderItem = frappe.qb.DocType("Work Order Item")
-    WorkOrder = frappe.qb.DocType("Work Order")
-    work_orders = (
-        frappe.qb.from_(WorkOrder)
-        .join(WorkOrderItem)
-        .on(WorkOrder.name == WorkOrderItem.parent)
-        .select(WorkOrder.name, WorkOrder.status)
-        .where(WorkOrderItem.item_code == production_item)
-        .where(WorkOrder.status == 'Not Started')
-    ).run()
-    
-    if len(work_orders):
-        return production_item
-    
-    return ""
+	stock_entry = frappe.get_doc("Stock Entry", stock_entry_name)
+
+	if stock_entry.docstatus != 1 or stock_entry.stock_entry_type != "Manufacture":
+		return ""
+
+	production_item = frappe.get_value("Work Order", stock_entry.work_order, "production_item")
+	WorkOrderItem = frappe.qb.DocType("Work Order Item")
+	WorkOrder = frappe.qb.DocType("Work Order")
+	work_orders = (
+		frappe.qb.from_(WorkOrder)
+		.join(WorkOrderItem)
+		.on(WorkOrder.name == WorkOrderItem.parent)
+		.select(WorkOrder.name, WorkOrder.status)
+		.where(WorkOrderItem.item_code == production_item)
+		.where(WorkOrder.status == "Not Started")
+	).run()
+
+	if len(work_orders):
+		return production_item
+
+	return ""
