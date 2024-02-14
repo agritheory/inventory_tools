d1e126b8dbaf09370f6d4c173e24af33ac994bd7
Francisco Roldán <franciscoproldan@gmail.com>
2024-02-14 18:33:05 -0300

    Prompt Material Transfer upon Completion of Manufacturing Stock Entry (#51)
    * feat: wip stock entry next action
    
    * feat: improve message
    
    * fix: on_submit hook
    
    * fix: parameter hardcoded
    
    * feat: change wording

--- inventory_tools/inventory_tools/overrides/stock_entry.py
+++ inventory_tools/inventory_tools/overrides/stock_entry.py
@@ -1,8 +1,7 @@
 import frappe
-from erpnext.stock.doctype.stock_entry.stock_entry import FinishedGoodError, StockEntry
 from frappe import _
 from frappe.utils import flt
-
+from erpnext.stock.doctype.stock_entry.stock_entry import FinishedGoodError, StockEntry
 from inventory_tools.inventory_tools.overrides.work_order import get_allowance_percentage
 
 
@@ -151,3 +150,30 @@ class InventoryToolsStockEntry(StockEntry):
 			frappe.msgprint(_("""All items have already been transferred for this Work Order."""))
 
 		return item_dict
+
+  
+@frappe.whitelist()
+def get_production_item_if_work_orders_for_required_item_exists(stock_entry_name: str) -> str:
+	stock_entry = frappe.get_doc("Stock Entry", stock_entry_name)
+
+	if stock_entry.docstatus != 1 or stock_entry.stock_entry_type != "Manufacture":
+		return ""
+
+	production_item = frappe.get_value("Work Order", stock_entry.work_order, "production_item")
+	WorkOrderItem = frappe.qb.DocType("Work Order Item")
+	WorkOrder = frappe.qb.DocType("Work Order")
+	work_orders = (
+		frappe.qb.from_(WorkOrder)
+		.join(WorkOrderItem)
+		.on(WorkOrder.name == WorkOrderItem.parent)
+		.select(WorkOrder.name, WorkOrder.status)
+		.where(WorkOrderItem.item_code == production_item)
+		.where(WorkOrder.status == "Not Started")
+	).run()
+
+	if len(work_orders):
+		return production_item
+
+	return ""
+
+
