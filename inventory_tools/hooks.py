89cc6a779cf07cc82af7263d63fc5da4430b17e9
ViralKansodiya-Fosserp <141210323+viralkansodiya@users.noreply.github.com>
2024-02-16 20:38:27 +0530

    Conflict of update solve

--- inventory_tools/hooks.py
+++ inventory_tools/hooks.py
@@ -34,7 +34,6 @@ doctype_js = {
 	"Work Order": "public/js/work_order_custom.js",
 	"Purchase Order": "public/js/purchase_order_custom.js",
 	"Purchase Invoice": "public/js/purchase_invoice_custom.js",
-	"Stock Entry": "public/js/stock_entry_custom.js",
 	"Job Card": "public/js/job_card_custom.js",
 	"Operation": "public/js/operation_custom.js",
 }
