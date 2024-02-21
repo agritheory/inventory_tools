71227cb1480a7c8d18ebf3cfd3625b57dff699eb
viral <viral@fosserp.com>
2024-02-21 09:22:30 +0000

    added a searchfield in query

--- inventory_tools/inventory_tools/overrides/workstation.py
+++ inventory_tools/inventory_tools/overrides/workstation.py
@@ -6,21 +6,31 @@ import frappe
 @frappe.whitelist()
 @frappe.validate_and_sanitize_search_inputs
 def get_alternative_workstations(doctype, txt, searchfield, start, page_len, filters):
+	if txt:
+		searchfields = frappe.get_meta(doctype).get_search_fields()
+		searchfields = " or ".join(["ws." + field + f" LIKE '%{txt}%'" for field in searchfields])
 	operation = filters.get("operation")
-	operation_doc = frappe.qb.DocType("Operation")
-	alternative_workstations = frappe.qb.DocType("Alternative Workstations")
-	qb_data = (
-		frappe.qb.from_(operation_doc)
-		.left_join(alternative_workstations)
-		.on(operation_doc.name == alternative_workstations.parent)
-		.select(alternative_workstations.workstation)
-		.where(operation_doc.name == operation)
+	conditions = ""
+
+	if txt and searchfields:
+		conditions = f"and ({searchfields})"
+
+	workstation = frappe.db.sql(
+		"""
+		Select aw.workstation, ws.workstation_type, ws.description
+		From `tabOperation` as op
+		Left Join `tabAlternative Workstations` as aw ON aw.parent = op.name
+		Left Join `tabWorkstation` as ws ON ws.name = aw.workstation
+		Where op.name = '{operation}' {conditions}
+	""".format(
+			conditions=conditions, operation=operation
+		)
 	)
-	workstation = frappe.db.sql(qb_data)
+
 	default_workstation = frappe.db.get_value("Operation", operation, "workstation")
 	flag = True
 	for row in workstation:
-		if None in row:
+		if row[0] == None:
 			workstation = ((default_workstation,),)
 			flag = False
 	if flag:
