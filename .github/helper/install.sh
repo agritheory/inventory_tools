f679cdd8e54275692fdde9eaed42aad644ee3afa
Tyler Matteson <tyler@agritheory.com>
2024-03-18 11:35:57 -0400

    ci: add app name

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -48,8 +48,8 @@ sed -i 's/schedule:/# schedule:/g' Procfile
 sed -i 's/socketio:/# socketio:/g' Procfile
 sed -i 's/redis_socketio:/# redis_socketio:/g' Procfile
 
-bench get-app https://github.com/frappe/hrms --branch "${BRANCH_NAME}" --resolve-deps --skip-assets
-bench get-app https://github.com/frappe/erpnext --branch "${BRANCH_NAME}" --resolve-deps --skip-assets --overwrite
+bench get-app erpnext https://github.com/frappe/erpnext --branch "${BRANCH_NAME}" --resolve-deps --skip-assets --overwrite
+bench get-app hrms https://github.com/frappe/hrms --branch "${BRANCH_NAME}" --resolve-deps --skip-assets --overwrite
 bench get-app inventory_tools "${GITHUB_WORKSPACE}" --skip-assets --resolve-deps
 
 printf '%s\n' 'frappe' 'erpnext' 'hrms' 'inventory_tools' > ~/frappe-bench/sites/apps.txt
