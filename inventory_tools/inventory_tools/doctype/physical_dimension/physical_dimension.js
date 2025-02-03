8c0da9ba63afb26f2c63a2c3cadc4cd4157056d7
Tyler Matteson <tyler@agritheory.com>
2025-02-03 12:07:25 -0500

    feat: remove case section, make dimensions non-negative

--- inventory_tools/inventory_tools/doctype/physical_dimension/physical_dimension.js
+++ inventory_tools/inventory_tools/doctype/physical_dimension/physical_dimension.js
@@ -9,36 +9,16 @@ frappe.ui.form.on('Physical Dimension', {
 			},
 		}))
 	},
-
 	item_height(frm) {
 		frm.trigger('calculate_item_volume')
 	},
-
 	item_length(frm) {
 		frm.trigger('calculate_item_volume')
 	},
-
 	item_width(frm) {
 		frm.trigger('calculate_item_volume')
 	},
-
-	case_height(frm) {
-		frm.trigger('calculate_case_volume')
-	},
-
-	case_length(frm) {
-		frm.trigger('calculate_case_volume')
-	},
-
-	case_width(frm) {
-		frm.trigger('calculate_case_volume')
-	},
-
 	calculate_item_volume(frm) {
 		frm.set_value('item_volume', frm.doc.item_length * frm.doc.item_width * frm.doc.item_height)
-	},
-
-	calculate_case_volume(frm) {
-		frm.set_value('case_volume', frm.doc.case_length * frm.doc.case_width * frm.doc.case_height)
 	},
 })
