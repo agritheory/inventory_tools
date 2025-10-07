281d708eb9d7f0f8bbb344baf4eaf1ecb455131f
Tyler Matteson <tyler@agritheory.com>
2025-10-07 09:42:39 -0400

    test: add fixtures for warehouse type

--- inventory_tools/tests/setup.py
+++ inventory_tools/tests/setup.py
@@ -207,11 +207,17 @@ def setup_manufacturing_settings(settings):
 
 def create_workstations():
 	for ws in workstations:
+		if not frappe.db.exists("Workstation Type", ws[2]):
+			wst = frappe.new_doc('Workstation Type')
+			wst.workstation_type = ws[2]
+			wst.save()
 		if frappe.db.exists("Workstation", ws[0]):
-			continue
-		work = frappe.new_doc("Workstation")
+			work = frappe.get_doc("Workstation", ws[0])
+		else:
+			work = frappe.new_doc("Workstation")
 		work.workstation_name = ws[0]
 		work.production_capacity = ws[1]
+		work.workstation_type = ws[2]
 		work.save()
 
 
